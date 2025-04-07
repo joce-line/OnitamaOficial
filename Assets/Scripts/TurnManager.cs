a1a4aef2c2c9e7449c0598b00dbe606fe358bbe3
joce-line <joceline.gutierrez@hotmail.com>
2025-04-07 01:24:20 -0300

    adição script para turno

---
+++ Assets/Scripts/TurnManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    public int playerTurn = 0;
+    public int activePlayer = 0;
+
+    void Start()
+    {
+        // Nada por enquanto
+    }
+
+    void Update()
+    {
+        // Pode ser usado para checar troca de turno futuramente
+    }
+}
