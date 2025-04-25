a87d14ca2d29033d473c5c2828e76574cde6a998
Thiago Hideki T <thiagohideki@outlook.com>
2025-04-25 00:02:13 -0300

    atualizacao background

---
+++ Assets/Scripts/BackGround.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    public Sprite bg00, bg01;//variaveis de Sprite background
+
+    public void Activate()
+    {
+        switch(this.name)
+        {
+            case "0": this.GetComponent<SpriteRenderer>().sprite = bg00;break;
+            case "1": this.GetComponent<SpriteRenderer>().sprite = bg01;break;
+        }
+        this.transform.position = new Vector3(0,0,-1);
+    }
+
+}
