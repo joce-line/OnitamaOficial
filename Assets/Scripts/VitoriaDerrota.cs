8282ba6fb7422b2f35dc36796a4f8c6e402a9ab7
joce-line <joceline.gutierrez@hotmail.com>
2025-05-01 20:14:39 -0300

    modificando logica de vit/der

---
+++ Assets/Scripts/VitoriaDerrota.cs
@@ -0,0 +1,40 @@
+using Assets.scripts.InfoPlayer;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VitoriaDerrota : MonoBehaviour
+{
+    public GameObject painelVitoria;
+    public GameObject painelDerrota;
+
+    void Start()
+    {
+        // Logica pega id do estatico para saber vencedor/perdedor
+        //TODO: mudar apos implementação multiplayer
+        int vencedor = DadosJogo.vencedor;
+        int perdedor = DadosJogo.perdedor;
+
+        if (vencedor == 1)
+        {
+            painelVitoria.SetActive(true);
+            painelDerrota.SetActive(false);
+        }
+        else if (vencedor == 2)
+        {
+            painelVitoria.SetActive(false);
+            painelDerrota.SetActive(true);
+        }
+    }
+
+    //TODO: modificar apos logica de paginas
+    public void Voltar()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    //TODO: modificar apos logica de paginas
+    public void Sair()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
