f76e0ecd8e93e04756ed4e5ac78eb8501c5377a5
joce-line <joceline.gutierrez@hotmail.com>
2025-05-19 18:42:26 -0300

    arrrumando codigo e removendo scripts desnecessarios

--- Assets/Scripts/Configuracao.cs
+++ Assets/Scripts/Configuracao.cs
@@ -4,16 +4,26 @@ using TMPro;
 using Assets.scripts.InfoPlayer;
 using System.Collections.Generic;
 
-public class ScriptTelaConfiguracao : MonoBehaviour
+public class Configuracao : MonoBehaviour
 {
     public TMP_Dropdown dropdownTemas;
 
-    public void voltarButton()
+    void Start()
+    {
+        // Verifica se a conexão está ativa
+        if (DatabaseManager.Instance == null || string.IsNullOrEmpty(DatabaseManager.ConnectionString))
+        {
+            Debug.LogError("DatabaseManager não inicializado.");
+            return;
+        }
+    }
+
+    public void VoltarButton()
     {
         SceneManager.LoadScene("MenuPrincipal");
     }
 
-    public void confirmarButton()
+    public void ConfirmarButton()
     {
         int valorSelecionado = dropdownTemas.value + 1;
         Debug.Log("ID do tema selecionado: " + valorSelecionado);
@@ -21,20 +31,6 @@ public class ScriptTelaConfiguracao : MonoBehaviour
         PlayerInfo.id_Background = valorSelecionado.ToString();
 
         BackgroundController.Instance?.AtualizarBackground(valorSelecionado);
-
-        // Verifica se a conexão está ativa
-        if (DatabaseManager.Instance == null || string.IsNullOrEmpty(DatabaseManager.ConnectionString))
-        {
-            Debug.LogError("DatabaseManager não inicializado.");
-            return;
-        }
-
-        // Verifica se a conexão está ativa
-        if (DatabaseManager.Instance == null || string.IsNullOrEmpty(DatabaseManager.ConnectionString))
-        {
-            Debug.LogError("DatabaseManager não inicializado.");
-            return;
-        }
 
         // Atualiza no banco de dados
         string updateQuery = "UPDATE usuarios SET id_Background = @bg WHERE idUsuario = @id";
