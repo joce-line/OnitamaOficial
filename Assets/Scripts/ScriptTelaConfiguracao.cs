cba2988acfd854da784c95463d167835782e2153
Thiago Hideki T <thiagohideki@outlook.com>
2025-05-25 22:12:36 -0300

    modificações na tela de configurações

--- Assets/Scripts/ScriptTelaConfiguracao.cs
+++ Assets/Scripts/ScriptTelaConfiguracao.cs
@@ -3,7 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class ScriptTelaConfiguração : MonoBehaviour
 {
-	//volta para a tela de menu
+	public GameObject panelConfig,panelDadosUsu;
+
+	//botões de seleção
+	public void configButton()
+	{
+		panelConfig.SetActive(true);
+		panelDadosUsu.SetActive(false);
+	}
+
+	public void dadoUsuButton()
+	{
+		panelConfig.SetActive(false);
+		panelDadosUsu.SetActive(true);
+	}
+
+	//navegação 
 	public void voltarButton()
 	{
 		SceneManager.LoadScene("MenuPrincipal");
