61ef12b723b5774771953e132c8c47658ea907d2
joce-line <joceline.gutierrez@hotmail.com>
2025-05-31 23:42:18 -0300

    testando conectividade

--- Assets/Scripts/CreateAndJoin.cs
+++ Assets/Scripts/CreateAndJoin.cs
@@ -9,6 +9,12 @@ public class CreateAndJoin : MonoBehaviourPunCallbacks
     public TMP_InputField input_Create;
     public TMP_InputField input_Join;
 
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
     public void CreateRoom()
     {
         PhotonNetwork.CreateRoom(input_Create.text, new RoomOptions() { MaxPlayers = 2, IsVisible = true, IsOpen = true });
@@ -21,11 +27,24 @@ public class CreateAndJoin : MonoBehaviourPunCallbacks
 
     public override void OnJoinedRoom()
     {
+        Debug.Log($"Player {PhotonNetwork.LocalPlayer.ActorNumber} entrou n asala");
         PhotonNetwork.LoadLevel("Game");
     }
 
     public void JoinRoomList(string RoomName)
     {
         PhotonNetwork.JoinRoom(RoomName);
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log($"Player {newPlayer.ActorNumber} entrou na salinha");
+    }
+
+    public bool IsRoomFull()
+    {
+        Debug.Log("IsRoomFull Player Count = " + PhotonNetwork.CurrentRoom.PlayerCount);
+        Debug.Log("IsRoomFull Max players = " + PhotonNetwork.CurrentRoom.MaxPlayers);
+        return PhotonNetwork.CurrentRoom.PlayerCount == PhotonNetwork.CurrentRoom.MaxPlayers;
     }
 }
